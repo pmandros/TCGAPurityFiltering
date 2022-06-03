2b59f3213f49e31cdb2b8efc4d30ad59a8a7cd41
Panagiotis Mandros <mandros.p@gmail.com>
2022-06-03 11:52:14 -0400

    Project commit

---
+++ R/TCGAPurityFiltering.R
@@ -0,0 +1,76 @@
+TCGAPurityFiltering=setRefClass("TCGAPurityFiltering",
+         fields = list(TCGA_purities= "data.frame"),
+         methods = list(
+           filter_assay = function(assay, cancer_type, filter_method, threshold){
+             if(class(assay) != "data.frame"){
+               stop("Error: Assay needs to be a data.frame")
+             }
+
+             if(!(filter_method %in% colnames(obj$TCGA_purities))){
+               stop("Error: Purity estimation method not found (misspelled maybe?). Options are ESTIMATE,
+                    ABSOLUTE, LUMP, IHC, CPE")
+             }
+
+             if(!(cancer_type %in% obj$TCGA_purities$Cancer_type)){
+               stop("Error: TCGA cancer type not found (misspelled maybe?)")
+             }
+
+             cancer_type_TCGA_purities <- obj$get_tissue_purities(cancer_type)
+
+             #for some reason the tcga tissue names are parsed with . instead of -
+             colnames(assay) <- gsub(pattern = "\\.", replacement = "-",colnames(assay))
+
+             not_in_assay <- rownames(cancer_type_TCGA_purities) %in% colnames(assay)
+             if(!all(not_in_assay)){
+               warning("Warning:  Sample names from purities file not found in TCGA tissue names.")
+             }
+
+             #remove samples not found in TCGA expression data
+             cancer_type_TCGA_purities<-cancer_type_TCGA_purities[which(not_in_assay==TRUE),]
+
+             #sort cancer_type_TCGA_purities according to TCGA assay order
+             p <- match_order(rownames(cancer_type_TCGA_purities), colnames(assay))
+             cancer_type_TCGA_purities <- cancer_type_TCGA_purities[p,]
+
+             #filter TCGA sample names according to threshold and filter_method
+             p <- which(cancer_type_TCGA_purities[[filter_method]]>=threshold)
+             dataframe_to_return <- assay[,p]
+             return(dataframe_to_return)
+           },
+           get_tissue_purities = function(cancer_type, filter_method=NULL){
+             if(!(cancer_type %in% obj$TCGA_purities$Cancer_type)){
+               stop("Error: TCGA cancer type not found (misspelled maybe?)")
+             }
+             cancer_type_TCGA_purities <- obj$TCGA_purities %>% subset(Cancer_type == cancer_type)
+             if(!is.null(filter_method)){
+               if(!(filter_method %in% colnames(obj$TCGA_purities))){
+                 stop("Error: Purity estimation method not found (misspelled maybe?). Options are ESTIMATE,
+                    ABSOLUTE, LUMP, IHC, CPE")
+               }
+               return(cancer_type_TCGA_purities %>% select(filter_method))
+             }
+             else{
+               return(cancer_type_TCGA_purities)
+             }
+
+           }
+         )
+)
+
+#' @export "CreateTCGAPurityFilteringObject"
+CreateTCGAPurityFilteringObject <- function(TCGA_purities_file="inst/extdata/purities.csv"){
+  fpath <- system.file("extdata", "purities.csv", package="TCGAPurityFiltering")
+  TCGA_purities <- read.csv(file = fpath, sep="\t", header=TRUE, row.names = 1)
+  TCGA_purities$X <-NULL
+  TCGA_purities$ESTIMATE <- as.numeric(gsub(",", ".", TCGA_purities$ESTIMATE))
+  TCGA_purities$ABSOLUTE <- as.numeric(gsub(",", ".", TCGA_purities$ABSOLUTE))
+  TCGA_purities$LUMP <- as.numeric(gsub(",", ".", TCGA_purities$LUMP))
+  TCGA_purities$IHC <- as.numeric(gsub(",", ".", TCGA_purities$IHC))
+  TCGA_purities$CPE <- as.numeric(gsub(",", ".", TCGA_purities$CPE))
+  s <- TCGAPurityFiltering$new(TCGA_purities = TCGA_purities)
+}
+
+
+
+
+
---
+++ example.R
@@ -0,0 +1,25 @@
+###install devtools package with install.packages("devtools")
+###then install the package with devtools::install() (provided you are at the project folder)
+
+
+library(TCGAPurityFiltering)
+
+# gene_exp_file <- "/Users/panos/git/graph_embed/expression_data/raw_LUAD.txt"
+# gene_expression <- read.csv(file = gene_exp_file, sep="\t", header=TRUE, row.names = 1)
+
+#create object from TCGAPurityFiltering class
+obj <- CreateTCGAPurityFilteringObject()
+
+#get the purities for whole TCGA
+TCGA_purities <-obj$TCGA_purities
+
+#get the purities of samples with LUAD (all estimation methods)
+LUAD_purities <-obj$get_tissue_purities(cancer_type = "LUAD")
+
+#get the purities of samples with LUAD estimated with CPE
+#possible methods are ESTIMATE, ABSOLUTE, LUMP, IHC, CPE")
+LUAD_purities_CPE <-obj$get_tissue_purities(cancer_type = "LUAD", filter_method = "CPE")
+
+#filter the gene expression of LUAD based on ESTIMATE purities and with threshold 0.7
+filtered_LUAD_assay_ESTIMATE <-obj$filter_assay(assay = gene_expression, cancer_type = "LUAD", filter_method =  "ESTIMATE", threshold = 0.7)
+
